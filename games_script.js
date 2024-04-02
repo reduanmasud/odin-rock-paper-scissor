69d0c6fee921f10e9defaefcca61c56a5beb829d
Reduan Masud <reduanmasudcse@gmail.com>
2024-04-02 22:11:44 +0600

    Play Game & Start Game & other function added and completed

--- games_script.js
+++ games_script.js
@@ -12,37 +12,63 @@ function playRound(playerSelection, computerSelection) {
         throw new Error("Please provide a valid string.");
         return;
     }
-    
+
     if(playerSelection == "ROCK")
     {
         if(computerSelection == "PAPER")
-            return "You Lose! Paper beats Rock";
+            return {"msg": "You Lose! Paper beats Rock", "score": -1};
         if(computerSelection == "ROCK")
-            return "TIE";
+            return {"msg": "TIE", "score": 0};
         if(computerSelection == "SCISSOR")
-            return "You Win! Rock breaks the Scissor";
+            return {"msg": "You Win! Rock breaks the Scissor", "score": 1};
 
     } else if(playerSelection == "PAPER" )
     {
         if(computerSelection == "ROCK")
-            return "You Win! Paper beats Rock";
+            return {"msg": "You Win! Paper beats Rock", "score": 1};
         if(computerSelection == "PAPER")
-            return "TIE";
+            return {"msg": "TIE", "score": 0};
         if(computerSelection == "SCISSOR")
-            return "You Lose! Scissor cuts the Paper";
+            return {"msg": "You Lose! Scissor cuts the Paper", "score": -1};
 
     } else {
         if(computerSelection == "ROCK")
-            return "You Lose! Rock breaks the Scissor";
+            return {"msg": "You Lose! Rock breaks the Scissor", "score": -1};
         if(computerSelection == "SCISSOR")
-            return "TIE";
+            return {"msg": "TIE", "score": 0};
         if(computerSelection == "PAPER")
-            return "You Win! Scissor cuts the Paper";
+            return {"msg": "You Win! Scissor cuts the Paper", "score": 1};
     }
     
 }
-  
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-  
+function startGame()
+{
+    let win = 0;
+    let fail = 0;
+
+    for (let index = 0; index < 5; index++) {
+        let yourSelection = prompt("Write what you choose!?");
+        console.log(yourSelection);
+        let res = playGame(yourSelection);
+        if(res == 1) win++;
+        if(res == -1) fail++;
+        console.log(`Your Score ${win} Computer Score ${fail}`);
+    }
+    
+    if(win > fail) console.log("Hurra! you WON");
+    if(win < fail) console.log("You Lost The Game");
+    if(win == fail) console.log("TIE")
+        
+}
+
+
+function playGame(yourSelection){
+    const computerSelection = getComputerChoice();
+    const res = playRound(yourSelection, computerSelection);
+    console.log(res.msg);
+    return res.score;
+}
+
+
+//Starting the game
+startGame();
